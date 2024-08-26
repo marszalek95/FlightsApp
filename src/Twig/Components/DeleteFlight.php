fd970ef0d9f0caf3c2c1d49531ae660943bf35ff
marszalek95 <marszal1995@gmail.com>
2024-08-26 14:19:33 +0200

    added deleting flight option

--- src/Twig/Components/DeleteFlight.php
+++ src/Twig/Components/DeleteFlight.php
@@ -2,19 +2,47 @@
 
 namespace App\Twig\Components;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use App\Entity\Flight;
+use App\Entity\FlightPrices;
 
 #[AsLiveComponent]
 class DeleteFlight extends AbstractController
 {
     use DefaultActionTrait;
 
+    #[LiveProp]
+    public int $id;
+
+    #[LiveProp]
+    public int $id_return;
+
     #[LiveAction]
-    public function deleteFlight()
+    public function deleteFlight(#[LiveArg] int $id, #[LiveArg] int $idReturn, EntityManagerInterface $entityManager)
     {
+
+        $flight = $entityManager->getRepository(Flight::class)->find($id);
+        $flightReturn = $entityManager->getRepository(Flight::class)->find($idReturn);
+        $prices = $entityManager->getRepository(FlightPrices::class)->findby(['flight_id' => $id]);
+        $pricesReturn = $entityManager->getRepository(FlightPrices::class)->findby(['flight_id' => $idReturn]);
+
+        $entityManager->remove($flight);
+        $entityManager->remove($flightReturn);
+        foreach ($prices as $price) {
+            $entityManager->remove($price);
+        }
+        foreach ($pricesReturn as $priceReturn) {
+            $entityManager->remove($priceReturn);
+        }
+
+        $entityManager->flush();
+
         return $this->redirectToRoute('app_flights');
 
     }
